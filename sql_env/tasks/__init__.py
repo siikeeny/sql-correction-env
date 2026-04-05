160959b76afea382a988c08c58d908cd64c86dd6
Sravani <sravaniamere@gmail.com>
2026-04-05 20:22:47 +0530

    add tasks init file

---
+++ sql_env/tasks/__init__.py
@@ -0,0 +1,11 @@
+from sql_env.tasks.easy import EASY_TASKS
+from sql_env.tasks.medium import MEDIUM_TASKS
+from sql_env.tasks.hard import HARD_TASKS
+
+ALL_TASKS = {
+    "easy": EASY_TASKS,
+    "medium": MEDIUM_TASKS,
+    "hard": HARD_TASKS,
+}
+
+TASK_SETS = ALL_TASKS
