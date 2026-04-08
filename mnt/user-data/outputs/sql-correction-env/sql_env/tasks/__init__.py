6464bfb6256d8d6f54f556d4aa41d530403e8e16
Sravani <sravaniamere@gmail.com>
2026-04-08 16:34:27 +0530

    fix remotes and remove stray folder

--- mnt/user-data/outputs/sql-correction-env/sql_env/tasks/__init__.py
+++
@@ -1,9 +0,0 @@
-from sql_env.tasks.easy import EASY_TASKS
-from sql_env.tasks.medium import MEDIUM_TASKS
-from sql_env.tasks.hard import HARD_TASKS
-
-ALL_TASKS = {
-    "easy": EASY_TASKS,
-    "medium": MEDIUM_TASKS,
-    "hard": HARD_TASKS,
-}
